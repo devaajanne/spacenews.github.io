cbf7d304962bad1cd57387ab17be2a76bf6e60cd
devaajanne <devaajanne@gmail.com>
2025-03-05 21:53:18 +0200

    Add scripts folder and utils file

---
+++ scripts/utils.js
@@ -0,0 +1,14 @@
+function setActiveSource(id) {
+  let element = document.getElementById(id);
+
+  if (element.classList.contains("btn-primary")) {
+    element.classList.remove("btn-primary");
+  } else {
+    let buttons = document.querySelectorAll(".sourceSelector");
+    buttons.forEach(function (button) {
+      button.classList.remove("btn-primary");
+    });
+
+    element.classList.add("btn-primary");
+  }
+}

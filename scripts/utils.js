9b62c366a1653d272767742291d6ef4915ed46a5
devaajanne <devaajanne@gmail.com>
2025-03-06 11:32:08 +0200

    Refactor utils script

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,15 +1,15 @@
 function setActiveSource(id, page) {
   let element = document.getElementById(id);
 
-  if (element.classList.contains("btn-primary")) {
-    element.classList.remove("btn-primary");
+  if (element.classList.contains("active")) {
+    element.classList.remove("active");
   } else {
     let buttons = document.querySelectorAll(".sourceSelector");
     buttons.forEach(function (button) {
-      button.classList.remove("btn-primary");
+      button.classList.remove("active");
     });
 
-    element.classList.add("btn-primary");
+    element.classList.add("active");
   }
 
   fetchNewsItems(id, page);
